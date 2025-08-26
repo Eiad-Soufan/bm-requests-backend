7afcafe078face0281304bb2d85194a87bc62810
Eiad-Soufan <eiad.soufan.2@gmail.com>
2025-08-26 16:57:27 +0800

    Update settings.py

--- model_system/settings.py
+++ model_system/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import dj_database_url
 from pathlib import Path
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -151,3 +152,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 # WhiteNoise settings
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
